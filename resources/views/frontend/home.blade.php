48da93793945eff74580e6add7874faa2c0058fa
SERAP <kayacan.serap@gmail.com>
2024-11-22 13:54:22 +0300

    created slider management .

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -1,18 +1,17 @@
 @extends('frontend.layouts.master')
 @section('content')
     <main>
-        <div class="">
-
+        <div>
             <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
                 <div class="carousel-inner">
                     <div class="carousel-item active">
-                        <img src="{{ asset('') }}assets/frontend/images/medical" class="d-block w-100" alt="...">
+                        <img src="{{ asset('') }}assets/frontend/images/carousel-1.webp" class="d-block w-100" alt="...">
                     </div>
                     <div class="carousel-item">
-                        <img src="..." class="d-block w-100" alt="...">
+                        <img src="{{ asset('') }}assets/frontend/images/carousel-2.webp" class="d-block w-100" alt="...">
                     </div>
                     <div class="carousel-item">
-                        <img src="..." class="d-block w-100" alt="...">
+                        <img src="{{ asset('') }}assets/frontend/images/carousel-3.jpg" class="d-block w-100" alt="...">
                     </div>
                 </div>
                 <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
@@ -24,7 +23,6 @@
                     <span class="visually-hidden">Next</span>
                 </button>
             </div>
-
         </div>
         <div class="main-wrapper-2">
             <div class="container">
