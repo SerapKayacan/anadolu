c7890358f951878fb945ef80a780e7b7a9da1a69
Osman Ustalar <osman.ustalar@dr.com.tr>
2024-11-22 14:00:54 +0300

    SEO Eklendi

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
+use Artesaos\SEOTools\Facades\SEOTools;
 use Illuminate\Http\Request;
 use App\Models\ServiceCategory;
 
@@ -11,6 +12,13 @@ class HomeController extends Controller
     {
         $serviceCategories = ServiceCategory::withCount('services')->orderBy('sort_order','ASC')->get();
         $types = ServiceCategory::types();
+
+        SEOTools::setTitle('Anasayfa'); // Dinamik olacak şekilde ayalanacak
+        SEOTools::setDescription('Özel Sultan Evde Sağlık Hizmetleri'); // Dinamik olacak şekilde ayalanacak
+        SEOTools::opengraph()->addProperty('type', 'website'); // Hizmet detay sayfasında type article olarak güncellenecek
+        SEOTools::metatags()->setKeywords(['doktor randevu', 'telemedicine']); // Dinamik olacak şekilde ayalanacak
+        SEOTools::addImages('https://sultanevdesaglikhizmetleri.com/wp-content/uploads/2021/10/cropped-sultan-logo-1536x288.png'); // Dinamik olacak şekilde ayalanacak
+
         return view('frontend.home', [
             "serviceCategories" => $serviceCategories,
             "types" => $types
--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {!! SEO::generate() !!}
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
@@ -19,7 +21,6 @@
     <link rel="stylesheet"  type="text/css" href="{{ asset('') }}assets/frontend/css/services-category.css">
     <link rel="stylesheet"  type="text/css" href="{{ asset('') }}assets/frontend/css/online-doctor.css">
     <link rel="stylesheet"  type="text/css" href="{{ asset('') }}assets/frontend/css/footer.css">
-
 </head>
 <body style="margin:0;padding:0;box-sizing:border-box; " >
 <header>
