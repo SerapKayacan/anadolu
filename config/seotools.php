a7713c2b887a84f926d5cf0368d5f9ac20c4a8b8
SERAP <kayacan.serap@gmail.com>
2025-03-17 00:42:09 +0300

    created the project

--- config/seotools.php
+++ config/seotools.php
@@ -8,27 +8,28 @@ return [
         /*
          * The default configurations to be used by the meta generator.
          */
-        'defaults'       => [
-            'title'        => "Anadolu Tadilat", // set false to total remove
-            'titleBefore'  => false, // Put defaults.title before page title, like 'It's Over 9000! - Dashboard'
-            'description'  => 'Tadilat Boya Epoxy Hizmeti — Hemen Arayın.' , // set false to total remove
-            'separator'    => ' - ',
-            'keywords'     => [
-                'tadilat', 'boya',
+        'defaults' => [
+            'title' => "Anadolu Tadilat", // set false to total remove
+            'titleBefore' => false, // Put defaults.title before page title, like 'It's Over 9000! - Dashboard'
+            'description' => 'Tadilat Boya Epoxy Hizmeti — Hemen Arayın.', // set false to total remove
+            'separator' => ' - ',
+            'keywords' => ['tadilat', 'boya', 'solventli epoksi reçine', 'solventsiz epoksi', 'su bazlı epoksi', 'solventli epoksi boya', 'solventsiz epoksi boya',
+                'su bazlı boya','yağlı boya','toz boya','saten boya','mat boya','dış cephe mantolama','iç cephe mantolama','su yalıtım sistemleri',
+                'ısı yalıtım sistemleri','ısı','epoksi','mantolama',
             ],
-            'canonical'    => null, // Set to null or 'full' to use Url::full(), set to 'current' to use Url::current(), set false to total remove
-            'robots'       => 'all', // Set to 'all', 'none' or any combination of index/noindex and follow/nofollow
+            'canonical' => null, // Set to null or 'full' to use Url::full(), set to 'current' to use Url::current(), set false to total remove
+            'robots' => 'all', // Set to 'all', 'none' or any combination of index/noindex and follow/nofollow
         ],
         /*
          * Webmaster tags are always added.
          */
         'webmaster_tags' => [
-            'google'    => null,
-            'bing'      => null,
-            'alexa'     => null,
+            'google' => null,
+            'bing' => null,
+            'alexa' => null,
             'pinterest' => null,
-            'yandex'    => null,
-            'norton'    => null,
+            'yandex' => null,
+            'norton' => null,
         ],
 
         'add_notranslate_class' => false,
@@ -38,12 +39,12 @@ return [
          * The default configurations to be used by the opengraph generator.
          */
         'defaults' => [
-            'title'       => 'Anadolu Tadilat', // set false to total remove
-            'description' => 'Tadilat Boya Epoxy Hizmeti — Hemen Arayın.' , // set false to total remove
-            'url'         => null, // Set null for using Url::current(), set false to total remove
-            'type'        => false,
-            'site_name'   => 'Anadolu Tadilat',
-            'images'      => [],
+            'title' => 'Anadolu Tadilat', // set false to total remove
+            'description' => 'Tadilat Boya Epoxy Hizmeti — Hemen Arayın.', // set false to total remove
+            'url' => null, // Set null for using Url::current(), set false to total remove
+            'type' => false,
+            'site_name' => 'Anadolu Tadilat',
+            'images' => [],
         ],
     ],
     'twitter' => [
@@ -51,7 +52,7 @@ return [
          * The default values to be used by the twitter cards generator.
          */
         'defaults' => [
-            'card'        => 'summary',
+            'card' => 'summary',
 //           'site'        => '@ÖzelSultanEvdeSağlıkHizmetleri',
         ],
     ],
@@ -60,11 +61,11 @@ return [
          * The default configurations to be used by the json-ld generator.
          */
         'defaults' => [
-            'title'       => 'Anadolu Tadilat', // set false to total remove
+            'title' => 'Anadolu Tadilat', // set false to total remove
             'description' => 'Tadilat Boya Epoxy Hizmeti — Hemen Arayın.', // set false to total remove
-            'url'         => null, // Set to null or 'full' to use Url::full(), set to 'current' to use Url::current(), set false to total remove
-            'type'        => 'WebPage',
-            'images'      => [],
+            'url' => null, // Set to null or 'full' to use Url::full(), set to 'current' to use Url::current(), set false to total remove
+            'type' => 'WebPage',
+            'images' => [],
         ],
     ],
 ];
