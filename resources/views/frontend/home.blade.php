b5602b47b096e479e596392aba1dbe512dc3b6d5
SERAP <kayacan.serap@gmail.com>
2025-03-17 22:13:24 +0300

    created the project

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -118,7 +118,7 @@
                             </div>
                             <div class="border border-5 border-light border-top-0 p-4">
                                 <p class="text-primary fw-medium mb-2">{{ $service->title }}</p>
-                                <h5 class="lh-base mb-0">{{ $service->category_page_detail }}</h5>
+                                <h4 class="lh-base mb-0">{{ $service->category_page_detail }}</h4>
                             </div>
                         </div>
                     </div>
