eefec5f8071fa88520a98e3c70d6dd87bc3a292f
SERAP <kayacan.serap@gmail.com>
2025-03-17 22:29:38 +0300

    created the project

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -94,10 +94,10 @@
                     </ul>
                 </div>
             </div>
-            <div class="row g-4 portfolio-container d-flex">
+            <div class="row g-4 portfolio-container">
                 @foreach ($services as $service)
-                    <div class="col-lg-4 col-md-6 d-flex">
-                        <div class="rounded overflow-hidden d-flex flex-column w-100">
+                    <div class="col-lg-4 col-md-6 portfolio-item category-{{ $service->category_id }} wow fadeInUp" data-wow-delay="0.5s">
+                        <div class="rounded overflow-hidden h-100 d-flex flex-column">
                             <div class="position-relative overflow-hidden">
                                 <img class="img-fluid w-100 h-auto"
                                      src="{{ $service->getFirstMediaUrl('images', 'large') ?: asset('default-image.jpg') }}"
@@ -117,17 +117,14 @@
                                     </a>
                                 </div>
                             </div>
-                            <!-- Ensures equal height for content inside each column -->
-                            <div class="border border-5 border-light p-4 d-flex flex-column flex-grow-1">
+                            <div class="border border-5 border-light border-top-0 p-4 d-flex flex-column flex-grow-1">
                                 <p class="text-primary fw-medium fs-3 mb-2">{{ $service->title }}</p>
-                                <h5 class="lh-base mb-0 flex-grow-1">{{ $service->category_page_detail }}</h5>
+                                <h5 class="lh-base mb-0">{{ $service->category_page_detail }}</h5>
                             </div>
                         </div>
                     </div>
                 @endforeach
             </div>
-
-
         </div>
     </div>
 
