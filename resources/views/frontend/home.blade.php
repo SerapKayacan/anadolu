7625910a7262ccccd65d505e61f35bcc93f0f27a
SERAP <kayacan.serap@gmail.com>
2025-03-17 23:04:18 +0300

    created the project

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -94,17 +94,17 @@
                     </ul>
                 </div>
             </div>
-            <div class="row g-4 portfolio-container">
+            <div class="row g-4 portfolio-container d-flex">
                 @foreach ($services as $service)
-                    <div class="col-lg-4 col-md-6 portfolio-item category-{{ $service->category_id }} wow fadeInUp" data-wow-delay="0.5s">
-                        <div class="rounded overflow-hidden h-100 d-flex flex-column">
+                    <div class="col-lg-4 col-md-6 portfolio-item category-{{ $service->category_id }} wow fadeInUp d-flex" data-wow-delay="0.5s">
+                        <div class="rounded overflow-hidden d-flex flex-column h-100">
                             <div class="position-relative overflow-hidden">
                                 <img class="img-fluid w-100 h-auto"
                                      src="{{ $service->getFirstMediaUrl('images', 'large') ?: asset('default-image.jpg') }}"
                                      alt="">
                             </div>
                             <div class="border border-5 border-light border-top-0 p-4 d-flex flex-column flex-grow-1">
-                                <p class="text-primary fw-medium fs-3 mb-2">{{ $service->title }}</p>
+                                <p class="text-primary fw-medium fs-4 mb-2">{{ $service->title }}</p>
                                 <h5 class="lh-base mb-0">{{ $service->category_page_detail }}</h5>
                                 <a class="fw-medium" href="{{ route('services-detail.show', ['slug' => $service->slug]) }}">Devamını Gör<i class="fa fa-arrow-right ms-2"></i></a>
                             </div>
@@ -112,6 +112,7 @@
                     </div>
                 @endforeach
             </div>
+
         </div>
     </div>
 
