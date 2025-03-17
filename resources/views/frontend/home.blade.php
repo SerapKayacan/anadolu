7f2006a5a22538ff2480c2658a2e53dc57b57e79
SERAP <kayacan.serap@gmail.com>
2025-03-17 23:10:07 +0300

    created the project

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -102,24 +102,11 @@
                                 <img class="img-fluid w-100 h-auto"
                                      src="{{ $service->getFirstMediaUrl('images', 'large') ?: asset('default-image.jpg') }}"
                                      alt="">
-                                <div class="portfolio-overlay">
-                                    <a class="btn btn-square btn-outline-light mx-1"
-                                       href="{{ $service->getFirstMediaUrl('images', 'large') ?: asset('default-image.jpg') }}"
-                                       data-lightbox="portfolio"
-                                       title=" {{ $service->title }} Görüntüle">
-                                        <i class="fa fa-eye" aria-hidden="true"></i>
-                                    </a>
-
-                                    <a class="btn btn-square btn-outline-light mx-1"
-                                       href="{{ route('services-detail.show', ['slug' => $service->slug]) }}"
-                                       title="{{ $service->title }} Görüntüle">
-                                        <i class="fa fa-link" aria-hidden="true"></i>
-                                    </a>
-                                </div>
                             </div>
                             <div class="border border-5 border-light border-top-0 p-4 d-flex flex-column flex-grow-1">
                                 <p class="text-primary fw-medium fs-3 mb-2">{{ $service->title }}</p>
                                 <h5 class="lh-base mb-0">{{ $service->category_page_detail }}</h5>
+                                <a class="fw-medium" href="{{ route('services-detail.show', ['slug' => $service->slug]) }}">Devamını Gör<i class="fa fa-arrow-right ms-2"></i></a>
                             </div>
                         </div>
                     </div>
