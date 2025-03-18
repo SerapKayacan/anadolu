2f1c82d91a290c7817ab2fea64f0b7ed601186b0
SERAP <kayacan.serap@gmail.com>
2025-03-18 22:13:21 +0300

    created the project

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -35,8 +35,6 @@
 
     </div>
     <!-- Footer End -->
-
-
     <!-- Back to Top -->
     <a href="#" class="btn btn-lg btn-primary btn-lg-square rounded-0 back-to-top"><i class="bi bi-arrow-up"></i></a>
 
