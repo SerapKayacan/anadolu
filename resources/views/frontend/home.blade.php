d2713d7ccb7524e541bbd56b2ddb4aad7c871d22
SERAP <kayacan.serap@gmail.com>
2025-03-18 20:10:34 +0300

    created the project

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -96,7 +96,7 @@
             </div>
             <div class="row g-4 portfolio-container">
                 @foreach ($services as $service)
-                    <div class="col-lg-4 col-md-6 portfolio-item category-{{ $service->category_id }} wow fadeInUp" data-wow-delay="0.5s">
+                    <div class="col-lg-4 col-md-6 portfolio-item category-{{ $service->category_id }} wow fadeInUp" data-wow-delay="0.2s">
                         <div class="rounded overflow-hidden h-100 d-flex flex-column">
                             <div class="position-relative overflow-hidden">
                                 <a href="{{ route('services-detail.show', ['slug' => $service->slug]) }}">
